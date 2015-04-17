f8509d5e709fce040058c3bad9f87682047a0c5a
Nuno Xu <nuno.xu@gmail.com>
2015-04-17 23:11:11 +0100

    Added agentScripts.

---
+++ AIForGames/Assets/Scripts/Agent/AgentHealth.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AgentHealth : MonoBehaviour
+{
+	public int startingHealth = 100;                            // The amount of health the player starts the game with.
+	public int currentHealth;                                   // The current health the player has.
+	public Slider healthSlider;                                 // Reference to the UI's health bar.
+	
+	Animator anim;                                              // Reference to the Animator component.
+	AudioSource agentAudio;                                    // Reference to the AudioSource component.
+	NavMeshAgent agentMovement;                              // Reference to the player's movement.
+	PlayerShooting agentShooting;                              // Reference to the PlayerShooting script.
+	bool isDead = false;                                                // Whether the player is dead.
+	bool damaged;                                               // True when the player gets damaged.
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent <Animator> ();
+		
+		// Set the initial health of the player.
+		currentHealth = startingHealth;
+	}
+	
+	
+	void Update ()
+	{
+		
+		// Reset the damaged flag.
+		damaged = false;
+	}
+	
+	
+	public void TakeDamage (int amount)
+	{
+		// Set the damaged flag so the screen will flash.
+		damaged = true;
+		
+		// Reduce the current health by the damage amount.
+		currentHealth -= amount;
+		
+		// Set the health bar's value to the current health.
+		healthSlider.value = currentHealth;
+		
+		// Play the hurt sound effect.
+		agentAudio.Play ();	
+		// If the player has lost all it's health and the death flag hasn't been set yet...
+		if(currentHealth <= 0 && !isDead)
+		{
+			// ... it should die.
+			Death ();
+		}
+	}
+	
+	
+	public void Death ()
+	{
+		// Set the death flag so this function won't be called again.
+		isDead = true;
+		
+		// Turn off any remaining shooting effects.
+		agentShooting.DisableEffects ();
+		
+		// Tell the animator that the player is dead.
+		anim.SetTrigger ("Die");
+		
+		// Turn off the movement and shooting scripts.
+		agentMovement.enabled = false;
+		agentShooting.enabled = false;
+	}
+	
+	public bool isHumanDead(){
+		return isDead;
+	}
+}
