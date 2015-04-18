fd55d6f876f484f98af33d5e574c16fe04465b5a
Nuno Xu <nuno.xu@gmail.com>
2015-04-18 11:00:19 +0100

    Player movement now properly follows camera orientation.

--- AIForGames/Assets/Scripts/Player/PlayerMovement.cs
+++ AIForGames/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,13 +3,16 @@
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 6f;            // The speed that the player will move at.
+	public Transform cameraHolderTransform;
 	
 	Vector3 movement;                   // The vector to store the direction of the player's movement.
 	Animator anim;                      // Reference to the animator component.
 	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
 	int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
 	float camRayLength = 100f;          // The length of the ray from the camera into the scene.
-	
+
+
+
 	void Awake ()
 	{
 		// Create a layer mask for the floor layer.
@@ -39,8 +42,11 @@ public class PlayerMovement : MonoBehaviour
 	
 	void Move (float h, float v)
 	{
+		Vector3 cameraForward = cameraHolderTransform.forward * v;
+		Vector3 cameraRight = cameraHolderTransform.right * h;
+
 		// Set the movement vector based on the axis input.
-		movement.Set (h, 0f, v);
+		movement = cameraForward + cameraRight;
 		
 		// Normalise the movement vector and make it proportional to the speed per second.
 		movement = movement.normalized * speed * Time.deltaTime;
