4efb63719dd738bd879d73d3ab514008db12643d
Nuno Xu <nuno.xu@gmail.com>
2015-04-18 16:23:01 +0100

    Changed UI, so that health and hunger is shown on top of agents.

---
+++ AIForGames/Assets/PlayerName.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerName : MonoBehaviour {
+
+	public Camera cam;
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (cam.transform);
+	}
+}
---
+++ AIForGames/Assets/Scripts/GUIObjectLabel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIObjectLabel : MonoBehaviour {
+	
+	public Transform target;  // Object that this label should follow
+	public Vector3 offset = Vector3.up;    // Units in world space to offset; 1 unit above object by default
+	public bool useMainCamera = true;   // Use the camera tagged MainCamera
+	public Camera cameraToUse ;   // Only use this if useMainCamera is false
+	Camera cam ;
+	Transform thisTransform;
+	Transform camTransform;
+	
+	void Start () 
+	{
+		thisTransform = transform;
+		if (useMainCamera)
+			cam = Camera.main;
+		else
+			cam = cameraToUse;
+		camTransform = cam.transform;
+	}
+	
+	
+	void Update()
+	{
+			thisTransform.position = cam.WorldToScreenPoint(target.position + offset);
+	}
+}
