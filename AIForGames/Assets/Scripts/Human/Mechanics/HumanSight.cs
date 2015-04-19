f91ef42952574d71245c86738e3f52b81e99641a
Tiago Santos <tiagomanuelfs@hotmail.com>
2015-04-19 19:30:38 +0100

    Hooman sight is now working properly

--- AIForGames/Assets/Scripts/Human/Mechanics/HumanSight.cs
+++ AIForGames/Assets/Scripts/Human/Mechanics/HumanSight.cs
@@ -3,42 +3,68 @@ using System.Collections;
 
 public class HumanSight : MonoBehaviour {
 
-
+	Ray shootRay;
+	RaycastHit shootHit;
+	float range = 20f;
+	int shootableMask;
 
 	HumanState humanState;
 	
 	void Awake(){
+		shootableMask = LayerMask.GetMask ("Shootable");
 		humanState = transform.root.GetComponent <HumanState> ();
 	}
 
 
 
 	void OnTriggerEnter (Collider other){
-		/*
-		Ray shootRay;
-		RaycastHit shootHit;
-		float range = 20f;
-		int shootableMask;
 
-		shootRay.origin = transform.position;
-		shootRay.direction = transform.position - other.transform.position;
 
-		if (Physics.Raycast (shootRay, out shootHit, range, shootableMask)) {
-		}*/
 
-		if (other.gameObject.tag == "Enemy") {
-			//adicionar a uma lista de inimigos?
-			//humanState.addEnemy(other.gameObject);
-		}
-		
 		if (other.gameObject.tag == "Food") {
 
-			humanState.sawFood = true;
-			//Fixme shoul add to list
-			humanState.foodSeen = other.gameObject;
+			if(isVisible (other)){
+				humanState.sawFood = true;
+				//Fixme shoul add to list
+				humanState.foodSeen = other.gameObject;
+
+			}
+
+
+
+
+
 		}
 
+		if (other.gameObject.tag == "Enemy") {
+			if(isVisible(other)){
+
+				//TODO: Tells you to run
+
+			}
+		}
+
+
+
+
+
 	}
+
+	bool isVisible (Collider other)
+	{
+		Vector3 direction = other.transform.position - transform.position;
+		if (Physics.Raycast (transform.position, direction.normalized, out shootHit, range, shootableMask)) {
+			//Debug.Log ("VI: Daqui: " + shootRay.origin + "para ali: " + other.transform.position + "vendo um " + shootHit.collider.gameObject);
+			if (shootHit.collider.gameObject.Equals (other.gameObject)) {
+				//Debug.Log ("VI MESMO um " + shootHit.collider.gameObject);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	//TODO: OnStay, se ficar mas a comida nao for visivel ele esquece-se
 
 	void OnTriggerExit(Collider other){
 
