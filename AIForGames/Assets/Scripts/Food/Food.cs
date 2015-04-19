976802c85e90f1a384de10f068032817e858dea1
Tiago Santos <tiagomanuelfs@hotmail.com>
2015-04-19 22:02:06 +0100

    Ammo behaviour working correctly now

--- AIForGames/Assets/Scripts/Food/Food.cs
+++ AIForGames/Assets/Scripts/Food/Food.cs
@@ -7,7 +7,6 @@ public abstract class Food : MonoBehaviour
 	public float foodPoints;
 
 	public virtual float eat(){
-		print("COMIDA AQUI COM " + foodPoints + "Pts!");
 		Object.Destroy (this.gameObject);
 		return foodPoints;
 	}
