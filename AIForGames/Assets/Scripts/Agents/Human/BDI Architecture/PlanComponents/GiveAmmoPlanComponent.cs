851c59b4d46652a219d240b37810b9b87b7075da
Tiago Santos <tiagomanuelfs@hotmail.com>
2015-05-15 07:25:46 +0100

    quick fix from 6am code made by a very confused programmer *there there*

--- AIForGames/Assets/Scripts/Agents/Human/BDI Architecture/PlanComponents/GiveAmmoPlanComponent.cs
+++ AIForGames/Assets/Scripts/Agents/Human/BDI Architecture/PlanComponents/GiveAmmoPlanComponent.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class GiveAmmoPlanComponent : PlanComponent
 {
 	Human destination;
-	int numBulelts = 0;
+	int numBullets = 0;
 	public GiveAmmoPlanComponent(Human human, int numBullets)
 		: base(human)
 	{
 		destination = human;
-		this.numBulelts = numBulelts;
+		this.numBullets = numBullets;
 	}
 	
 	
 	public override bool TryExecuteAction()
 	{
-		human.Actuators.GiveAmmo(numBulelts);
+		human.Actuators.GiveAmmo(human, numBullets);
 		
 		return true;
 	}
